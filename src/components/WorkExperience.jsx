1d79bcecb10e235ae28d774dc55b5ba933c6ec08
shuaibkashmiri <114304104+shuaibkashmiri@users.noreply.github.com>
2024-09-03 21:36:07 +0530

    fixes

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -8,7 +8,14 @@ const WorkExperience = () => {
     {EXPERIENCES.map((experience,index)=>{
         <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
             <div className="w-full lg:w-1/4">
-            <p>{experience.year}</p>
+            <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
+            </div>
+            <div className="w-full max-w-xl lg:w-3/4">
+<h6 className="mb-2 font-semibold">
+  {experience.role} -{""} <span className="text-sm text-purple-100">
+    {experience.company}
+  </span>
+</h6>
 
             </div>
 
